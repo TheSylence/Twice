c215760f0a259d805af85281a2b2d3a0f74b67a6
Matthias Specht <sylence@btbsoft.org>
2016-04-01 21:28:30 +0200

    Added Source Monitor project and command file

--- releasify.ps1
+++ releasify.ps1
@@ -7,3 +7,12 @@ Invoke-Expression "squirrel --releasify=$nupkg -b=$channel --no-msi --icon=Twice
 
 Remove-Item $nupkg
 Move-Item Releases\Setup.exe Releases\twice-setup.exe
+
+$version = ($nupkg -split "(\d+\.\d+\.\d+(\.\d+)?)")[1]
+Copy-Item smcmd.xml smcmd.tmp.xml
+(Get-Content smcmd.tmp.xml).replace('@CHECKPOINT_NAME@', $version) | Set-Content smcmd.tmp.xml
+
+$sm = "C:\Program Files (x86)\SourceMonitor\SourceMonitor.exe"
+Invoke-Expression "& '$sm' /C smcmd.tmp.xml"
+
+Remove-Item smcmd.tmp.xml
