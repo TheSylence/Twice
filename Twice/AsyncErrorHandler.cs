51c9e2130e8e186d87eaf7e9f07de38048da1a9a
Matthias Specht <sylence@btbsoft.org>
2016-04-03 17:45:00 +0200

    Added AsyncErrorHandler to log async exceptions

---
+++ Twice/AsyncErrorHandler.cs
@@ -0,0 +1,15 @@
+﻿using Anotar.NLog;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Twice
+{
+	[ExcludeFromCodeCoverage]
+	public static class AsyncErrorHandler
+	{
+		public static void HandleException( Exception exception )
+		{
+			LogTo.FatalException( "Exception in async code", exception );
+		}
+	}
+}
