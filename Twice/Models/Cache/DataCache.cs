feb56c3fd83dad2c29ff4e9fa1976995bc8636e5
Matthias Specht <sylence@btbsoft.org>
2016-05-16 20:41:00 +0200

    Added simple cache library

--- Twice/Models/Cache/DataCache.cs
+++ Twice/Models/Cache/DataCache.cs
@@ -1,66 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reactive.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SimpleCache;
 
 namespace Twice.Models.Cache
 {
 	internal class DataCache : IDataCache
 	{
-		public DataCache()
+		public DataCache( ICache cache )
 			: this( Constants.IO.CacheFileName )
 		{
+			Cache = cache;
 		}
 
 		public DataCache( string fileName )
 		{
-			
 		}
 
 		public async Task AddHashtag( string hashtag )
 		{
 			var data = new HashtagCacheEntry( hashtag );
-			await Cache.InsertObject( data.GetKey(), data, DateTimeOffset.Now.Add( Constants.Cache.HashtagExpiration ) );
+			var key = data.GetKey();
+			var value = JsonConvert.SerializeObject( data );
+
+			await Cache.Set( key, value, Constants.Cache.HashtagExpiration );
 		}
 
 		public async Task AddUser( ulong id, string name )
 		{
 			UserCacheEntry data = new UserCacheEntry( id, name );
+			var key = data.GetKey();
+			var value = JsonConvert.SerializeObject( data );
 
-			await Cache.InsertObject( data.GetKey(), data, DateTimeOffset.Now.Add( Constants.Cache.UserInfoExpiration ) );
+			await Cache.Set( key, value, Constants.Cache.UserInfoExpiration );
 		}
 
 		public async Task<IEnumerable<string>> GetKnownHashtags()
 		{
-			var result = await Cache.GetAllObjects<HashtagCacheEntry>();
+			var keys = ( await Cache.GetKeys() ).Where( k => k.StartsWith( "HASHTAG:", StringComparison.Ordinal ) );
+			var result = new List<string>();
 
-			return result.Select( r => r.Hashtag );
+			foreach( var key in keys )
+			{
+				var json = await Cache.Get( key );
+				var tag = JsonConvert.DeserializeObject<HashtagCacheEntry>( json );
+
+				result.Add( tag.Hashtag );
+			}
+
+			return result;
 		}
 
 		public async Task<IEnumerable<ulong>> GetKnownUserIds()
 		{
-			var result = await Cache.GetAllObjects<UserCacheEntry>();
+			var keys = ( await Cache.GetKeys() ).Where( k => k.StartsWith( "USER:", StringComparison.Ordinal ) );
+			var result = new List<ulong>();
 
-			return result.Select( u => u.Id );
+			foreach( var key in keys )
+			{
+				var json = await Cache.Get( key );
+				var user = JsonConvert.DeserializeObject<UserCacheEntry>( json );
+
+				result.Add( user.Id );
+			}
+
+			return result;
 		}
 
 		public async Task<IEnumerable<UserCacheEntry>> GetKnownUsers()
 		{
-			return await Cache.GetAllObjects<UserCacheEntry>();
+			var keys = ( await Cache.GetKeys() ).Where( k => k.StartsWith( "USER:", StringComparison.Ordinal ) );
+			var result = new List<UserCacheEntry>();
+
+			foreach( var key in keys )
+			{
+				var json = await Cache.Get( key );
+				var user = JsonConvert.DeserializeObject<UserCacheEntry>( json );
+
+				result.Add( user );
+			}
+
+			return result;
 		}
 
 		public async Task<LinqToTwitter.Configuration> ReadTwitterConfig()
 		{
-			return await Cache.GetObject<LinqToTwitter.Configuration>( ConfigurationKey )
-				.Catch( Observable.Return<LinqToTwitter.Configuration>( null ) );
+			var json = await Cache.Get( ConfigurationKey );
+			if( json == null )
+			{
+				return null;
+			}
+
+			return JsonConvert.DeserializeObject<LinqToTwitter.Configuration>( json );
 		}
 
 		public async Task SaveTwitterConfig( LinqToTwitter.Configuration cfg )
 		{
-			await Cache.InsertObject( ConfigurationKey, cfg, TimeSpan.FromDays( 1 ) );
+			var json = JsonConvert.SerializeObject( cfg );
+			await Cache.Set( ConfigurationKey, json, TimeSpan.FromDays( 1 ) );
 		}
 
 		private const string ConfigurationKey = "twitter.help.configuration";
+		private readonly ICache Cache;
 	}
 }
