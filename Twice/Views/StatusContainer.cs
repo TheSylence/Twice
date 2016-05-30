e1c3cb95f2f7e5c94c2fd94af2cc9c5a58cfd951
Matthias Specht <sylence@btbsoft.org>
2016-05-30 11:47:42 +0200

    Selected tweet is now highlighted in detail view. Closes #5

--- Twice/Views/StatusContainer.cs
+++ Twice/Views/StatusContainer.cs
@@ -5,21 +5,6 @@ namespace Twice.Views
 {
 	public class StatusContainer : ContentControl
 	{
-		public static readonly DependencyProperty DisplayIndicatorsProperty =
-			DependencyProperty.Register( "DisplayIndicators", typeof(bool), typeof(StatusContainer), new PropertyMetadata( true ) );
-
-		public static readonly DependencyProperty DisplaySourceProperty =
-			DependencyProperty.Register( "DisplaySource", typeof(bool), typeof(StatusContainer), new PropertyMetadata( false ) );
-
-		public static readonly DependencyProperty EnableHoverIconsProperty =
-			DependencyProperty.Register( "EnableHoverIcons", typeof(bool), typeof(StatusContainer), new PropertyMetadata( true ) );
-
-		public static readonly DependencyProperty ShowFavoritesProperty =
-			DependencyProperty.Register( "ShowFavorites", typeof(bool), typeof(StatusContainer), new PropertyMetadata( false ) );
-
-		public static readonly DependencyProperty ShowRetweetsProperty =
-			DependencyProperty.Register( "ShowRetweets", typeof(bool), typeof(StatusContainer), new PropertyMetadata( false ) );
-
 		public bool DisplayIndicators
 		{
 			get { return (bool)GetValue( DisplayIndicatorsProperty ); }
@@ -38,6 +23,12 @@ namespace Twice.Views
 			set { SetValue( EnableHoverIconsProperty, value ); }
 		}
 
+		public bool HightlightStatus
+		{
+			get { return (bool)GetValue( HightlightStatusProperty ); }
+			set { SetValue( HightlightStatusProperty, value ); }
+		}
+
 		public bool ShowFavorites
 		{
 			get { return (bool)GetValue( ShowFavoritesProperty ); }
@@ -49,5 +40,23 @@ namespace Twice.Views
 			get { return (bool)GetValue( ShowRetweetsProperty ); }
 			set { SetValue( ShowRetweetsProperty, value ); }
 		}
+
+		public static readonly DependencyProperty DisplayIndicatorsProperty =
+			DependencyProperty.Register( "DisplayIndicators", typeof( bool ), typeof( StatusContainer ), new PropertyMetadata( true ) );
+
+		public static readonly DependencyProperty DisplaySourceProperty =
+			DependencyProperty.Register( "DisplaySource", typeof( bool ), typeof( StatusContainer ), new PropertyMetadata( false ) );
+
+		public static readonly DependencyProperty EnableHoverIconsProperty =
+			DependencyProperty.Register( "EnableHoverIcons", typeof( bool ), typeof( StatusContainer ), new PropertyMetadata( true ) );
+
+		public static readonly DependencyProperty HightlightStatusProperty =
+			DependencyProperty.Register( "HightlightStatus", typeof( bool ), typeof( StatusContainer ), new PropertyMetadata( false ) );
+
+		public static readonly DependencyProperty ShowFavoritesProperty =
+			DependencyProperty.Register( "ShowFavorites", typeof( bool ), typeof( StatusContainer ), new PropertyMetadata( false ) );
+
+		public static readonly DependencyProperty ShowRetweetsProperty =
+			DependencyProperty.Register( "ShowRetweets", typeof( bool ), typeof( StatusContainer ), new PropertyMetadata( false ) );
 	}
 }
