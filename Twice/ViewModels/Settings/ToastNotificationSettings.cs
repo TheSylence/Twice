a30dd5cd3ab3439df557a63a4f0b91cf5db65923
Matthias Specht <sylence@btbsoft.org>
2016-06-16 12:13:24 +0200

    Added preview for in-app notifications

--- Twice/ViewModels/Settings/ToastNotificationSettings.cs
+++ Twice/ViewModels/Settings/ToastNotificationSettings.cs
@@ -5,9 +5,10 @@ namespace Twice.ViewModels.Settings
 {
 	internal class ToastNotificationSettings : NotificationModuleSettings
 	{
-		public ToastNotificationSettings( IConfig config )
+		public ToastNotificationSettings( IConfig config, INotifier notifier )
 		{
 			Enabled = config.Notifications.ToastsEnabled;
+			Notifier = notifier;
 		}
 
 		public override void SaveTo( IConfig config )
@@ -15,6 +16,17 @@ namespace Twice.ViewModels.Settings
 			config.Notifications.ToastsEnabled = Enabled;
 		}
 
+		protected override bool CanExecutePreviewCommand()
+		{
+			return true;
+		}
+
+		protected override void ExecutePreviewCommand()
+		{
+			Notifier.DisplayMessage( "This is a test notification", NotificationType.Information );
+		}
+
 		public override string Title => Strings.InAppNotification;
+		private readonly INotifier Notifier;
 	}
 }
