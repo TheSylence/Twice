3fd968b0d9918cf5f77424fb11a9d009f505eb11
Matthias Specht <sylence@btbsoft.org>
2016-06-16 10:00:37 +0200

    Clicking column buttons now scroll to the associated column

---
+++ Twice/Behaviors/ScrollIntoView.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace Twice.Behaviors
+{
+	internal class ScrollIntoView : Behavior<Button>
+	{
+		protected override void OnAttached()
+		{
+			AssociatedObject.Click += AssociatedObject_Click;
+		}
+
+		private void AssociatedObject_Click( object sender, RoutedEventArgs e )
+		{
+			if( Item != null && Control != null )
+			{
+				var element = Control.ItemContainerGenerator.ContainerFromItem( Item ) as FrameworkElement;
+				if( element != null )
+				{
+					element.BringIntoView();
+					e.Handled = true;
+				}
+			}
+		}
+
+		public ItemsControl Control
+		{
+			get { return (ItemsControl)GetValue( ControlProperty ); }
+			set { SetValue( ControlProperty, value ); }
+		}
+
+		public object Item
+		{
+			get { return GetValue( ItemProperty ); }
+			set { SetValue( ItemProperty, value ); }
+		}
+
+		public static readonly DependencyProperty ControlProperty =
+			DependencyProperty.Register( "Control", typeof( ItemsControl ), typeof( ScrollIntoView ), new PropertyMetadata( null ) );
+
+		public static readonly DependencyProperty ItemProperty =
+			DependencyProperty.Register( "Item", typeof( object ), typeof( ScrollIntoView ), new PropertyMetadata( null ) );
+	}
+}
