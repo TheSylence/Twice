b86157768a8a5994d01f1cc26bec7b86c4797954
Matthias Specht <sylence@btbsoft.org>
2016-06-27 13:26:46 +0200

    Account names with underscores are now correctly displayed. Fixes #81

---
+++ Twice/Converters/MnemonicsRemover.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Twice.Converters
+{
+	internal class MnemonicsRemover : IValueConverter
+	{
+		public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+		{
+			string strValue = value as string;
+			if( strValue == null )
+			{
+				return value;
+			}
+
+			return strValue.Replace( "_", "__" );
+		}
+
+		public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
