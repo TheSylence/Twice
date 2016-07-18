dac6bbf5b6ae9ef53288b023912fbe6bca00bfc8
Matthias Specht <sylence@btbsoft.org>
2016-07-18 15:12:43 +0200

    Added tests for status creation in scheduling. Closes #55

--- Twice/Models/Scheduling/SchedulerJob.cs
+++ Twice/Models/Scheduling/SchedulerJob.cs
@@ -5,6 +5,13 @@ namespace Twice.Models.Scheduling
 {
 	internal class SchedulerJob
 	{
+		public SchedulerJob()
+		{
+			AccountIds = new List<ulong>();
+			FilesToAttach = new List<string>();
+			IdsToDelete = new List<ulong>();
+		}
+
 		public List<ulong> AccountIds { get; set; }
 		public List<string> FilesToAttach { get; set; }
 		public List<ulong> IdsToDelete { get; set; }
