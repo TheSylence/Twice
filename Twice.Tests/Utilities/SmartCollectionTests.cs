846ca7ee8e788304fdb2dfef408ef3eef19ffbde
Matthias Specht <sylence@btbsoft.org>
2016-07-28 12:16:59 +0200

    Improved code coverage

---
+++ Twice.Tests/Utilities/SmartCollectionTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Twice.Utilities;
+
+namespace Twice.Tests.Utilities
+{
+	[TestClass, ExcludeFromCodeCoverage]
+	public class SmartCollectionTests
+	{
+		[TestMethod, TestCategory( "Utilities" )]
+		public void CollectionCanBeConstructedFromEnumerable()
+		{
+			// Arrange
+			var enumerable = Enumerable.Range( 0, 5 );
+
+			// Act
+			var collection = new SmartCollection<int>( enumerable );
+
+			// Assert
+			CollectionAssert.AreEqual( enumerable.ToArray(), collection.ToArray() );
+		}
+
+		[TestMethod, TestCategory( "Utilities" )]
+		public void ResetReplacesContent()
+		{
+			// Arrange
+			var collection = new SmartCollection<int>
+			{
+				1,2,3
+			};
+
+			// Act
+			collection.Reset( new[] { 4, 5, 6 } );
+
+			// Assert
+			CollectionAssert.AreEqual( new[] { 4, 5, 6 }, collection.ToArray() );
+		}
+
+		[TestMethod, TestCategory( "Utilities" )]
+		public void CollectionCanBeConstructedFromList()
+		{
+			// Arrange
+			var list = new List<int> {3, 6, 3, 2};
+
+			// Act
+			var collection = new SmartCollection<int>( list );
+
+			// Assert
+			CollectionAssert.AreEqual( list.ToArray(), collection.ToArray() );
+		}
+
+		[TestMethod, TestCategory( "Utilities" )]
+		public void EmptyConstructorCreatesEmptyList()
+		{
+			// Arrange Act
+			var collection = new SmartCollection<int>();
+
+			// Assert
+			Assert.AreEqual( 0, collection.Count );
+		}
+	}
+}
---
+++ Twice.Tests/ViewModels/Info/InfoDialogViewModelTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Twice.ViewModels.Info;
+
+namespace Twice.Tests.ViewModels.Info
+{
+	[TestClass, ExcludeFromCodeCoverage]
+	public class InfoDialogViewModelTests
+	{
+		[TestMethod, TestCategory( "ViewModels.Info" )]
+		public void ChangelogIsCorrectlyLoaded()
+		{
+			// Arrange Act
+			var vm = new InfoDialogViewModel();
+
+			// Assert
+			Assert.IsTrue( vm.Changelogs.Any() );
+		}
+
+		[TestMethod, TestCategory( "ViewModels.Info" )]
+		public void LicenseInformationIsCorrectlyLoaded()
+		{
+			// Arrange Act
+			var vm = new InfoDialogViewModel();
+
+			// Assert
+			Assert.IsTrue( vm.Licenses.Any() );
+		}
+	}
+}
