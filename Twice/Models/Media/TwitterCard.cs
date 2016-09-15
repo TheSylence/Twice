1f442640d10d6658c648c3ec7e79a6e3318f2d9b
Matthias Specht <sylence@btbsoft.org>
2016-09-15 17:17:26 +0200

    Added classes for extracting twitter cards from websites

---
+++ Twice/Models/Media/TwitterCard.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace Twice.Models.Media
+{
+	internal class TwitterCard
+	{
+		public TwitterCard( Dictionary<string, string> meta )
+		{
+			string temp;
+
+			if( meta.TryGetValue( "twitter:card", out temp ) )
+			{
+				switch( temp )
+				{
+				case "summary":
+					Type = CardType.Summary;
+					break;
+
+				case "summary_large_image":
+					Type = CardType.SummaryLargeImage;
+					break;
+
+				default:
+					Type = CardType.Unknown;
+					break;
+				}
+			}
+
+			if( meta.TryGetValue( "twitter:site", out temp ) )
+			{
+				Site = temp;
+			}
+
+			if( meta.TryGetValue( "twitter:title", out temp ) )
+			{
+				Title = temp;
+			}
+
+			if( meta.TryGetValue( "twitter:description", out temp ) )
+			{
+				Description = temp;
+			}
+
+			if( meta.TryGetValue( "twitter:image", out temp ) )
+			{
+				Image = temp;
+			}
+			if( meta.TryGetValue( "twitter:image:alt", out temp ) )
+			{
+				ImageAlt = temp;
+			}
+		}
+
+		public string Description { get; }
+		public string Image { get; }
+		public string ImageAlt { get; }
+
+		public bool IsValid => !string.IsNullOrWhiteSpace( Title )
+		                       && !string.IsNullOrWhiteSpace( Description )
+		                       && !string.IsNullOrWhiteSpace( Site )
+		                       && Type != CardType.Unknown;
+
+		public string Site { get; }
+		public string Title { get; }
+		public CardType Type { get; }
+	}
+}
---
+++ Twice/Models/Media/TwitterCardExtractor.cs
@@ -0,0 +1,52 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Twice.Models.Media
+{
+	internal class TwitterCardExtractor : ITwitterCardExtractor
+	{
+		public async Task<TwitterCard> ExtractCard( Uri url )
+		{
+			using( var client = new HttpClient() )
+			{
+				var response = await client.GetAsync( "http://www.golem.de/news/notebook-dell-verpasst-dem-xps-13-einen-groesseren-akku-1609-123274.html" );
+				var str = await response.Content.ReadAsStringAsync();
+
+				return ExtractCard( str );
+			}
+		}
+
+		private TwitterCard ExtractCard( string http )
+		{
+			int headEnd = http.IndexOf( "</head>", StringComparison.OrdinalIgnoreCase );
+			string html = http.Substring( 0, headEnd ) + "</html>";
+			html = WebUtility.HtmlDecode( html );
+
+			var doc = new HtmlDocument();
+			doc.LoadHtml( html );
+
+			var head = doc.DocumentNode.SelectSingleNode( "html/head" );
+			var metaNodes = head.SelectNodes( "meta" );
+			Dictionary<string, string> twitterMetaInfo = new Dictionary<string, string>();
+
+			foreach( var meta in metaNodes )
+			{
+				var name = meta.Attributes["name"]?.Value;
+				var value = meta.Attributes["content"]?.Value;
+
+				if( name?.StartsWith( "twitter:", StringComparison.OrdinalIgnoreCase ) == true && value != null )
+				{
+					twitterMetaInfo.Add( name, value );
+				}
+			}
+
+			return new TwitterCard( twitterMetaInfo );
+		}
+
+		internal static ITwitterCardExtractor Default { get; } = new TwitterCardExtractor();
+	}
+}
