d8163374d74d084634e9bb186dbedb9ba69f9a84
Matthias Specht <sylence@btbsoft.org>
2017-02-04 23:41:34 +0100

    Removed Source Monitor project from source

--- releasify.ps1
+++ releasify.ps1
@@ -10,18 +10,3 @@ Remove-Item Releases\Setup.exe
 Invoke-Expression "squirrel --releasify=$nupkg --no-msi --icon=Twice\Resources\TwitterIcon.ico"
 
 Remove-Item $nupkg
-
-$version = ($nupkg -split "(\d+\.\d+\.\d+(\.\d+)?)")[1]
-$baseDir = Split-Path $MyInvocation.MyCommand.Path
-$projectFile = Join-Path $baseDir "Twice.smproj"
-$sourceDir = $baseDir
-
-Copy-Item smcmd.xml smcmd.tmp.xml
-(Get-Content smcmd.tmp.xml).replace('@CHECKPOINT_NAME@', $version) | Set-Content smcmd.tmp.xml
-(Get-Content smcmd.tmp.xml).replace('@PROJECT_FILE@', $projectFile) | Set-Content smcmd.tmp.xml
-(Get-Content smcmd.tmp.xml).replace('@SOURCE_DIR@', $sourceDir) | Set-Content smcmd.tmp.xml
-
-$sm = "C:\Program Files (x86)\SourceMonitor\SourceMonitor.exe"
-Start-Process $sm -argumentlist "/C smcmd.tmp.xml" -wait
-
-Remove-Item smcmd.tmp.xml
