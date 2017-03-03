42ca7538292474f629dd760b9cc7de15d2297936
Matthias Specht <sylence@btbsoft.org>
2017-03-03 23:16:14 +0100

    Centering window now uses correct positons for all displays. Fixes #137

--- Twice/Behaviors/WindowHelper.cs
+++ Twice/Behaviors/WindowHelper.cs
@@ -1,25 +1,36 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using Application = System.Windows.Application;
 
 namespace Twice.Behaviors
 {
 	/// <summary>
-	///  Helper methods for working with WPF Windows 
+	///     Helper methods for working with WPF Windows
 	/// </summary>
 	[ExcludeFromCodeCoverage]
 	internal static class WindowHelper
 	{
 		/// <summary>
-		///  Centers the specified window at its parent or, if it has no parent, at the screen. 
+		///     Centers the specified window at its parent or, if it has no parent, at the screen.
 		/// </summary>
 		/// <param name="window"> The window to center </param>
 		public static void Center( Window window )
 		{
-			var parentWidth = window.Owner?.ActualWidth ?? SystemParameters.WorkArea.Width;
-			var parentHeight = window.Owner?.ActualHeight ?? SystemParameters.WorkArea.Height;
-			var parentLeft = window.Owner?.WindowState == WindowState.Maximized ? 0 : window.Owner?.Left ?? 0;
-			var parentTop = window.Owner?.WindowState == WindowState.Maximized ? 0 : window.Owner?.Top ?? 0;
+			if( window == null )
+			{
+				return;
+			}
+
+			var mainWindow = Application.Current.MainWindow;
+			var actualPos = GetActualWindowPosition( mainWindow );
+
+			var parentWidth = mainWindow.ActualWidth;
+			var parentHeight = mainWindow.ActualHeight;
+			var parentLeft = actualPos.X;
+			var parentTop = actualPos.Y;
 
 			var width = window.ActualWidth;
 			var height = window.ActualHeight;
@@ -29,7 +40,23 @@ namespace Twice.Behaviors
 		}
 
 		/// <summary>
-		///  Tries to set the <see cref="Window.DialogResult" /> of a window and fails silently. 
+		/// Returns the ACTUAL position of a window, even if it is maximized
+		/// </summary>
+		/// <param name="window"></param>
+		/// <returns></returns>
+		static Point GetActualWindowPosition( Window window )
+		{
+			if( window.WindowState != WindowState.Maximized )
+			{
+				return new Point( window.Left, window.Top );
+			}
+
+			var windowScreen = Screen.FromHandle( new WindowInteropHelper( window ).Handle );
+			return new Point( windowScreen.Bounds.Left, windowScreen.Bounds.Top );
+		}
+
+		/// <summary>
+		///     Tries to set the <see cref="Window.DialogResult" /> of a window and fails silently.
 		/// </summary>
 		/// <param name="window"> The window to set the result for </param>
 		/// <param name="result"> The result to set </param>
