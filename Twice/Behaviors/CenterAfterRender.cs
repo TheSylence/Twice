b16a481cc3d62843af43d1e473e299fdd619f5a7
Matthias Specht <sylence@btbsoft.org>
2017-03-05 17:47:44 +0100

    Dialog centering is now upated when dialog content is updated

---
+++ Twice/Behaviors/CenterAfterRender.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+
+namespace Twice.Behaviors
+{
+	class CenterAfterRender : BehaviorBase<Window>
+	{
+		protected override void OnAttached()
+		{
+			base.OnAttached();
+
+			AssociatedObject.LayoutUpdated += AssociatedObject_EventHandler;
+			AssociatedObject.LocationChanged += AssociatedObject_LocationChanged;
+		}
+
+		protected override void OnCleanup()
+		{
+			AssociatedObject.LayoutUpdated -= AssociatedObject_EventHandler;
+		}
+
+		private void AssociatedObject_EventHandler( object sender, EventArgs e )
+		{
+			if( ShouldCenter )
+			{
+				WindowHelper.Center( AssociatedObject );
+			}
+		}
+
+		private void AssociatedObject_LocationChanged( object sender, EventArgs e )
+		{
+			ShouldCenter = false;
+		}
+
+		private bool ShouldCenter = true;
+	}
+}
